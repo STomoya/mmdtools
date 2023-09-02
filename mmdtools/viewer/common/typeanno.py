f0bf1e62a2068b37c3fe531ba7805fce5efb4cb2
Tomoya Sawada <stomoya0110@gmail.com>
2023-09-02 13:11:31 +0900

    🎉 bone

---
+++ mmdtools/viewer/common/typeanno.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from typing import TypeAlias
+
+__all__ = [
+    'Vector3D',
+    'Vector4D'
+]
+
+
+Vector3D: TypeAlias = 'tuple[float, float, float]'
+Vector4D: TypeAlias = 'tuple[float, float, float, float]'
